7a728d9f66ec9e89a5b6d54e3f7dfd1c07c264a1
ArmandoKatito10 <armandokatito10@gmail.com>
2021-07-21 13:50:05 +0100

    add Database Mysql and inser data

---
+++ config/BD/Portal.sql
@@ -0,0 +1,13 @@
+create database portal_noticias
+default character set utf8
+default collate utf8_general_ci;
+ CREATE TABLE IF NOT EXISTS noticias(
+     id_noticia INT NOT NULL AUTO_INCREMENT,
+     titulo VARCHAR (100),
+     noticia TEXT,
+     data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     PRIMARY KEY (id_noticia)
+ )CHARSET = utf8;
+ INSERT INTO noticias VALUES ()
+
+ INSERT INTO noticias(titulo,noticia)VALUES('Título da Notícia','Notícia em si');

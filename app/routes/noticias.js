7826336b9214551dc184359e551132f193c70da6
ArmandoKatito10 <armandokatito10@gmail.com>
2021-07-25 23:46:25 +0100

    Banco de dados Conectado

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,23 +1,26 @@
 
-module.exports= function(app){
+module.exports = function (app) {
 
-// criando rotas para arquivos HTML esternos
-app.get('/noticias', function(req, res){
-        var mysql = require('mysql');
+	// criando rotas para arquivos HTML esternos
+	app.get('/noticias', function (req, res) {
+		var mysql = require('mysql2');
 
-        var connection = mysql.createConnection({
-            host : 'localhost',
-            user : 'Armando',
-            password : 'DataScience@10k',
-            database : 'portal_noticias'
-        });
-        connection.query('select * from noticias',function(error, result){
-        res.send(result);
-        });
-
-});
+		var connection = mysql.createConnection({
+			host: 'localhost',
+			user: 'Armando',
+			password: 'DataScience@10k',
+			database: 'portal_noticias'
+		});
+		//console.log(connection)
+		connection.query('select * from noticias', function (error, result) {
+			if(error){
+				throw error;
+			}
+			//console.log(result)
+			res.send(result);
+		});
+	});
 
 };
    // res.render("noticias/noticias");
 
-  

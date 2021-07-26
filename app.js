a2f7c0a424e739caf2457e9cd9aa8467782ddbe5
ArmandoKatito10 <armandokatito10@gmail.com>
2021-07-26 15:46:00 +0100

    add consign to control'selfs routs

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var app = require('./config/server');
-var rotaNoticias = require('./app/routes/noticias')(app);
- var routHome = require('./app/routes/home')(app);
+//var rotaNoticias = require('./app/routes/noticias')(app);
+ //var routHome = require('./app/routes/home')(app);
 
- var routformu = require('./app/routes/formulario_inclusao_noticias')(app);
+ //var routformu = require('./app/routes/formulario_inclusao_noticias')(app);
 
 app.listen(3000, function(){
     console.log("servidor rodando com o Express");
--- config/server.js
+++ config/server.js
@@ -1,5 +1,6 @@
 // configurações do servidor 
 var express = require('express');
+var consign = require('consign');
 
 // chamada da funcão express
 var app = express();
@@ -7,5 +8,8 @@ var app = express();
 app.set('view engine', 'ejs');
 //dir pattern to serch all views from app
 app.set('views', './app/views');
+//Add consign routes to all files server app
+consign().include('app/routes').into(app);
+
 // retornando a variável app
 module.exports = app;
